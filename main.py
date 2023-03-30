d8c210b0c262e8212eff05de2f69d5e01cead3a8
m0rosha <maksik2k08@gmail.com>
2023-03-30 19:28:37 +0300

    lmao1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from pygame import *
-main_win = display.set_mode((700,500))
+win = display.set_mode((700,500))
+bkgrnd_img = image.load('kosmos-mlechnyy-put-zvezdy.jpg')
+bkgrnd = transform.scale(bkgrnd_img,(700,500))
 fps = 60
 clock = time.Clock()
+
 display.set_caption('Ping Pong')
 running = True
 while running:
+    win.blit(bkgrnd,(0,0))
+    
     for a in event.get():
         if a.type == QUIT:
             running = False
