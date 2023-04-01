8e1f3d5d6a8752dd0d4200df1a0eb0fdd268c09f
m0rosha <101067195+m0rosha@users.noreply.github.com>
2023-04-01 18:31:40 +0200

    Update main.py
    added players

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ bkgrnd = transform.scale(bkgrnd_img,(700,500))
 fps = 60
 clock = time.Clock()
 class GameSprite(sprite.Sprite):
-    def __init__(self, image,x,y,width,height,speed):
-        self.image = image
+    def __init__(self, image_name,x,y,width,height,speed):
+        self.image = transform.scale(image.load(image_name),(width,height))
         self.rect = self.image.get_rect()
-        self.rect.x = rect.x
-        self.rect.y = rect.y
+        self.rect.x = x
+        self.rect.y = y
         self.width = width
         self.height = height
         self.speed = speed
@@ -28,13 +28,19 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys [K_DOWN]:
             self.rect.y += self.speed 
-
-
+player1 = Player('wood.png', 20, 200,50,200,5)
+player2 = Player('wood.png',500,200,50,200,5)
+ball = GameSprite('ball.png',200,200,50,200,5)
 
 display.set_caption('Ping Pong')
 running = True
 while running:
     win.blit(bkgrnd,(0,0))
+    player2.reset()
+    player1.reset()
+    player1.update_1()
+    player2.update_2()
+
     
     for a in event.get():
         if a.type == QUIT:
